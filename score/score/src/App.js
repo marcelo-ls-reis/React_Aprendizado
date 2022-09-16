d965fe7ce980dabe8bbeaed08c89f773b8f10760
marcelo-ls-reis <marcelo.reis5@fatec.sp.gov.br>
2022-09-16 20:08:34 +0000

    Novo projeto aprendizado

---
+++ score/score/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const logo = "Score Jogos"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{logo}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
